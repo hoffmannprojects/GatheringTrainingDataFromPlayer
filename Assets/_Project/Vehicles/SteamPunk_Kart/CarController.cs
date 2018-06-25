b95dde209ce8860c2513426a640375ac8be6d86d
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-06-25 13:37:04 +0200

    Added starter project package.

---
+++ Assets/_Project/Vehicles/SteamPunk_Kart/CarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+
+public class CarController : MonoBehaviour 
+{
+    [SerializeField] private float speed = 100f;
+    [SerializeField] private float rotationSpeed = 100.0F;
+
+    private void Update ()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
